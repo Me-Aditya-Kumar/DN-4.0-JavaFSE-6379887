726494112035f950a79eb6b4e300285cf39b8b48
Aditya Kumar <adityakumar2004jsr@gmail.com>
2025-06-22 11:53:52 +0530

    22/06/2025 11:53

--- Week 1/Design Principles and Patterns/Exercise 3/BuilderPatternTest.java
+++ Week 1/Design Principles and Patterns/Exercise 3/BuilderPatternTest.java
@@ -3,8 +3,6 @@ public class BuilderPatternTest {
 
         Computer basicComputer = new Computer.Builder("Intel i5", "8GB")
                                         .build();
-
-        // Advanced configuration
         Computer gamingComputer = new Computer.Builder("Intel i9", "32GB")
                                         .setStorage("1TB SSD")
                                         .setGraphicsCard("NVIDIA RTX 4080")
