0cdac581661d227b923f27c02da80431feed91ba
Aditya Kumar <adityakumar2004jsr@gmail.com>
2025-07-23 02:08:17 +0530

    23/07/2025 02:08

---
+++ Week 5/Microservices with API Gateway/api-gateway/src/main/java/com/cognizant/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.cognizant.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.cognizant.apigateway.filter.AuthFilter;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+    @Bean
+public AuthFilter authFilter() {
+    return new AuthFilter();
+}
+}
---
+++ Week 5/Microservices with API Gateway/api-gateway/src/main/java/com/cognizant/api_gateway/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package com.cognizant.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GatewayFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.equals("Bearer secret-token")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
